ea3b09fcbb6631caba5e3319424aa8d7a296d55e
Zach Lee <zclee5@gmail.com>
2020-08-29 21:24:31 -0400

    v8 initial commit. added user and comment associations

---
+++ v8/changelog.md
@@ -0,0 +1,5 @@
+# v8 Changelog
+
+2020.08.29
+* Associated users and comments
+    - Forced saving and displaying author's name to a comment automatically
---
+++ v8/routes/comments.js
@@ -0,0 +1,56 @@
+const express = require("express"),
+    router = express.Router({mergeParams: true}),
+    Campground = require("../models/campground"),
+    Comment = require("../models/comment");
+;
+
+// Comments NEW
+router.get("/new", isLoggedIn, (req, res) => {
+    // find campground by id
+    Campground.findById(req.params.id, (err, campground) => {
+        if(err){
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
+        }
+    })
+})
+
+// Comments CREATE
+router.post("/", isLoggedIn, (req, res) => {
+    // look up campground using id
+    Campground.findById(req.params.id, (err, campground) => {
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            // create new comment
+            Comment.create(req.body.comment, (err, comment) => {
+                if(err) {
+                    console.log(err);
+                } else {
+                    // add username and id to comment
+                    comment.author.id = req.user._id;
+                    comment.author.username = req.user.username;
+                    // save comment
+                    comment.save();
+                    // connect new comment to campground  
+                    campground.comments.push(comment);
+                    campground.save();
+                    // redirect to show page of that campground
+                    res.redirect('/campgrounds/' + campground._id);
+                }
+            })
+        }
+    })
+})
+
+// Middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+};
+
+module.exports = router;
